41a92292298c5d282972bf4ef2b3bb98e9ac3988
unknownpgr <unknownpgr@gmail.com>
2020-09-01 07:32:30 +0900

    Add comments

--- src/recognize/lane-detection.cpp
+++ src/recognize/lane-detection.cpp
@@ -6,24 +6,26 @@
 #define W VPE_OUTPUT_W
 #define H VPE_OUTPUT_H
 
+// This is possible because W(=VPE_OUT_W) and H(=VPE_OUTPUT_H) are actually constants defined with preprocessor.
+// Therefore, dynamic memory allocation is not requried.
+unsigned char raw[W * H * 3];
+
 void detect_lane(recog_arg *arg, vector_lane *result)
 {
     // Copy image
-    unsigned char *raw = new unsigned char[W * H * 3];
     std::copy(arg->camera_output, arg->camera_output + W * H * 3, raw);
+    // Just wrap raw data with object
     cv::Mat src(H, W, CV_8UC3, raw);
 
     // Convert image to grayscale
     cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
 
-    int index = 0;
-    for (int i = 0; i < src.cols; i++)
-    {
-        index = W * i + 10;
-        src.data[index] = 0;
-    }
+    // Do image processing
+    // someComplicatedMagincalImageProcessing(src,result)
 
-    // Convert grayscale image back to RGB image and display
+    // Convert grayscale image back to RGB image
     cv::cvtColor(src, src, cv::COLOR_GRAY2BGR);
+
+    // Copy processed image to display
     std::copy(src.data, src.data + W * H * 3, arg->display_input);
 }
