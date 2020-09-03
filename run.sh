715ad00b7c7de14823cc8dcc2781f2d725a761a5
unknownpgr <unknownpgr@gmail.com>
2020-09-03 09:56:00 +0900

    Fix run.sh annotation

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-echo "Build source"
+echo "Build the source"
 make
 
-echo "Copy object file to remote device"
+echo "Copy the objects file to the remote device"
 scp "build/ctrlboard" root@10.10.70.4:"/home/root/bin/ctrlboard"
 scp "build/recognize" root@10.10.70.4:"/home/root/bin/recognize"
 
-echo "Execute object file on remote device"
+echo "Initialize ths IPCs on the remote device."
 ssh -t root@10.10.70.4 "ipcrm -Q 123;ipcrm -M 456"
+
+echo "Execute program on the remote device"
 ssh -t root@10.10.70.4 "/home/root/bin/ctrlboard 123 & /home/root/bin/recognize 456 123;"
