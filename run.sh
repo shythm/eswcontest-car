685c5a7139051f1826d8c567b70a5250a71d082b
unknownpgr <unknownpgr@gmail.com>
2020-09-03 09:41:45 +0900

    Optimize CMake

--- run.sh
+++ run.sh
@@ -1,18 +1,9 @@
 echo "Build source"
-
-cd shared
-make clean
 make
-cd ../
-
-cd recognize
-make clean
-make
-cd ../
 
 echo "Copy object file to remote device"
-scp "shared/ctrlboard" root@10.10.70.4:"/home/root/bin/ctrlboard"
-scp "recognize/recognize" root@10.10.70.4:"/home/root/bin/recognize"
+scp "build/ctrlboard" root@10.10.70.4:"/home/root/bin/ctrlboard"
+scp "build/recognize" root@10.10.70.4:"/home/root/bin/recognize"
 
 echo "Execute object file on remote device"
 ssh -t root@10.10.70.4 "ipcrm -Q 123;ipcrm -M 456"
