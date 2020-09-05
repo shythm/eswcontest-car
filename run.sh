1b6c783811d888436db150e21e5bd6a0f9eb1334
unknownpgr <unknownpgr@gmail.com>
2020-09-05 19:29:27 +0900

    Optimize run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ echo "Build the source"
 make
 
 echo "Copy the objects file to the remote device"
-scp "build/ctrlboard" root@10.10.70.4:"/home/root/bin/ctrlboard"
-scp "build/recognize" root@10.10.70.4:"/home/root/bin/recognize"
-scp "build/clean-car" root@10.10.70.4:"/home/root/bin/clean-car"
+scp "build/ctrlboard" root@10.10.70.4:"bin/"
+scp "build/recognize" root@10.10.70.4:"bin/"
+scp "build/clean-car" root@10.10.70.4:"bin/"
 
 echo "Execute the programs on the remote device"
-ssh -t root@10.10.70.4 "/home/root/bin/ctrlboard & /home/root/bin/recognize"
+ssh -t root@10.10.70.4 "bin/ctrlboard & bin/recognize"
 
 echo "Clean the programs on the remote device."
-ssh -t root@10.10.70.4 "/home/root/bin/clean-car"
+ssh -t root@10.10.70.4 "bin/clean-car"
