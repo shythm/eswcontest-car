7f7c201fb1b6f90210e7106abf406ea53cb876dc
unknownpgr <unknownpgr@gmail.com>
2020-09-05 14:40:56 +0900

    Implement first positioning algorithm

--- src/recognize/lane-detection.cpp
+++ src/recognize/lane-detection.cpp
@@ -7,6 +7,7 @@
 #define H VPE_OUTPUT_H
 
 using namespace cv;
+using namespace std;
 
 // This is possible because W(=VPE_OUT_W) and H(=VPE_OUTPUT_H) are actually constants defined with preprocessor.
 // Therefore, dynamic memory allocation is not requried.
@@ -14,13 +15,18 @@ unsigned char raw[W * H * 3];
 Size size_origin = Size(VPE_OUTPUT_W, VPE_OUTPUT_H);
 Size size_small = Size(VPE_OUTPUT_W / 2, VPE_OUTPUT_H / 2);
 
-float vanish = 0;
-float range = 300;
-float viewRange = 0.15f;
+double vanish = 0;      // Y position of vanish point
+double range = 300;     //
+double viewRange = 0.4; // Region of interest
 
 Scalar blue(255, 0, 0);
 Scalar red(0, 0, 255);
 Scalar green(0, 255, 0);
+Scalar yellow(0, 255, 255);
+
+int cent = 100, ran = 30;
+Scalar hsvLow(cent - ran, 100, 0);
+Scalar hsvHigh(cent + ran, 255, 255);
 
 void _getPerspectiveTransform(Mat *M, Point2f *src)
 {
@@ -31,9 +37,10 @@ void _getPerspectiveTransform(Mat *M, Point2f *src)
     // x1 = (W/2+range)*(y-vanish)/(H-vanish)+W/2
     // x2 = W/2-(W/2+range)*(y-vanish)/(H-vanish)
 
-    float wh = W * 1.0f / 2;
-    float y = H * viewRange;
-    float xDelta = (wh + range) * (y - vanish) / (H - vanish);
+    // I don't know why but getPerspeciveTransform does not work properly when all Point2f positions are integer.
+    double wh = W / 2 + 0.0001;
+    double y = viewRange * H;
+    double xDelta = (wh + range) * (y - vanish) / (H - vanish);
 
     src[0] = Point2f(wh - xDelta, y);
     src[1] = Point2f(wh + xDelta, y);
@@ -52,61 +59,112 @@ void _getPerspectiveTransform(Mat *M, Point2f *src)
 void detect_lane(recog_arg *arg, vector_lane *result)
 {
     static bool init = true;
-    static Mat M;
+    static Mat M, mask;
     static vector<vector<Point>> roi;
     static int npt[] = {4};
     if (init)
     {
         init = false;
-        std::cout << size_origin << "," << size_small << std::endl;
+        cout << size_origin << "," << size_small << endl;
         Point2f pts[4];
         vector<Point> _roi;
         _getPerspectiveTransform(&M, pts);
         for (int i = 0; i < 4; i++)
             _roi.push_back(pts[i]);
         roi.push_back(_roi);
-        std::cout << M << std::endl;
+        cout << M << endl;
     }
 
     // Copy image and wrap raw data with Mat object
-    std::copy(arg->camera_output, arg->camera_output + W * H * 3, raw);
-    Mat src(H, W, CV_8UC3, raw);
+    copy(arg->camera_output, arg->camera_output + W * H * 3, raw);
+    Mat img(H, W, CV_8UC3, raw);
 
     // Draw roi
-    polylines(src, roi, true, red, 3);
+    // polylines(img, roi, true, red, 3);
 
-    // Perspective transform
-    warpPerspective(src, src, M, Size(W, H));
+    cvtColor(img, img, COLOR_BGR2GRAY);
 
-    // Convert image to grayscale
-    // cvtColor(src, src, COLOR_BGR2YUV);
-    // resize(src, src, size_small, 0, 0, CV_INTER_NN);
+    warpPerspective(img, img, M, Size(W, H));
+
+    threshold(img, mask, 1, 128, THRESH_BINARY_INV);
+
+    add(img, mask, img);
+
+    adaptiveThreshold(
+        img, img, 255,
+        CV_ADAPTIVE_THRESH_MEAN_C,
+        CV_THRESH_BINARY, 31, -30);
+
+    int
+        left = 0,
+        right = 0,
+        left_n = 0,
+        right_n = 0;
+
+    for (int i = 0; i < H; i++)
+    {
+        uchar *pointer_input = img.ptr<uchar>(i);
+        for (int j = 0; j < W / 2; j++)
+        {
+            if (pointer_input[j] > 128)
+            {
+                left += j;
+                left_n++;
+            }
+        }
+        for (int j = W / 2; j < W; j++)
+        {
+            if (pointer_input[j] > 128)
+            {
+                right += j;
+                right_n++;
+            }
+        }
+    }
+
+    if (left_n > 0)
+        left /= left_n;
+
+    if (right_n > 0)
+        right /= right_n;
+    else
+        right = W - 1;
+
+    static float center = 0;
+    static float gain_irr = 0.95;
+    center = center * gain_irr + (left + right) * (1 - gain_irr) / 2;
+
+    // resize(img, img, size_small, 0, 0, CV_INTER_NN);
 
     // Threshold frame
-    // inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+    // inRange(img, hsvLow, hsvHigh, mask);
 
-    // adaptiveThreshold(
-    //     src, src, 255,
-    //     CV_ADAPTIVE_THRESH_MEAN_C,
-    //     CV_THRESH_BINARY, 3, -10);
+    // static int counter = 0;
+    // if (counter % 10 == 0)
+    // {
+    //     string fileName = "sc_" + to_string(counter + 10) + ".png";
+    //     cout << fileName << endl;
+    //     imwrite(fileName, img);
+    // }
+    // counter++;
 
-    // Convert grayscale image back to RGB image
+    // Get nonzero values
+    // Mat locations;
+    // findNonZero(img, locations);
+    // cout << locations << endl;
 
-    // resize(src, src, size_origin, 0, 0, CV_INTER_NN);
-    // cvtColor(src, src, COLOR_YUV2BGR);
+    // img.copyTo(img, mask);
+    cvtColor(img, img, COLOR_GRAY2BGR);
+    // resize(img, img, size_origin, 0, 0, CV_INTER_NN);
 
-    // line(src,
-    //          Point(W / 2, vanish),
-    //          Point(-range, H),
-    //          Scalar(255, 0, 0));
-
-    // line(src,
-    //          Point(W / 2, vanish),
-    //          Point(W + range, H),
-    //          Scalar(0, 0, 255));
-
-    //
+    // Draw center line
+    line(img, Point(W / 2, 0), Point(W / 2, H), blue);
+    line(img, Point(left, 0), Point(left, H), red);
+    line(img, Point(right, 0), Point(right, H), green);
+    line(img, Point(center, 0), Point(center, H), yellow);
 
     // Copy processed image to display
-    std::copy(src.data, src.data + W * H * 3, arg->display_input);
+    copy(img.data, img.data + W * H * 3, arg->display_input);
+
+    // result->position = center - W / 2;
 }
