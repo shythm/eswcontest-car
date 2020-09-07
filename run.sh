77b8e030d1b85de5633247a66214f114f6c9c3cc
unknownpgr <unknownpgr@gmail.com>
2020-09-08 03:02:04 +0900

    Add control execution line

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ echo "Copy the objects file to the remote device"
 scp "build/ctrlboard" root@10.10.70.4:"bin/"
 scp "build/recognize" root@10.10.70.4:"bin/"
 scp "build/clean-car" root@10.10.70.4:"bin/"
+scp "build/control"   root@10.10.70.4:"bin/"
 
 echo "Execute the programs on the remote device"
-ssh -t root@10.10.70.4 "bin/ctrlboard & bin/recognize"
+ssh -t root@10.10.70.4 "bin/ctrlboard & bin/recognize & bin/control"
 
 echo "Clean the programs on the remote device."
 ssh -t root@10.10.70.4 "bin/clean-car"
