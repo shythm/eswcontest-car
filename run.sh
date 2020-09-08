fa3e9ca2f7653b06d022b83b81266f75579a518f
unknownpgr <unknownpgr@gmail.com>
2020-09-08 21:16:32 +0900

    Fix run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ make
 echo "Copy the objects file to the remote device"
 scp "build/ctrlboard" root@10.10.70.4:"bin/"
 scp "build/recognize" root@10.10.70.4:"bin/"
-scp "build/drive" root@10.10.70.4:"bin/"
 scp "build/clean-car" root@10.10.70.4:"bin/"
 scp "build/control"   root@10.10.70.4:"bin/"
 
--- src/process/mission-drive.c
+++ src/process/mission-drive.c
@@ -1,46 +1,42 @@
+#include "ctrlboard-lib.h"
 #include "process.h"
 
 void init_drive(State *state) {
-    ctrlboard_byte_container container;
+    ctrlboard_byte_container data;
 
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_CAMERA_Y_SERVO_CONTROL,
-                          CMD_TYPE_WRITE, 2, &container) != MSG_STATE_SUCCESS) {
+    if (ctrl_msgq(CMD_CAMERA_Y_SERVO_CONTROL, 2, &data) != MSG_STATE_SUCCESS) {
         printf("fail 1");
     }
 
-    container.c_uint8 = 0;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_POSITION_CONTROL_ON_OFF,
-                          CMD_TYPE_WRITE, 1, &container) != MSG_STATE_SUCCESS) {
+    data.c_uint8 = 0;
+    if (ctrl_msgq(CMD_POSITION_CONTROL_ON_OFF, 1, &data) != MSG_STATE_SUCCESS) {
         printf("fail 2");
     }
 
-    container.c_uint8 = 1;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_SPEED_CONTROL_ON_OFF,
-                          CMD_TYPE_WRITE, 1, &container) != MSG_STATE_SUCCESS) {
+    data.c_uint8 = 1;
+    if (ctrl_msgq(CMD_SPEED_CONTROL_ON_OFF, 1, &data) != MSG_STATE_SUCCESS) {
         printf("fail 3");
     }
 
-    container.c_uint8 = 20;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_SPEED_PID_PROPORTIONAL,
-                          CMD_TYPE_WRITE, 1, &container) != MSG_STATE_SUCCESS) {
+    data.c_uint8 = 20;
+    if (ctrl_msgq(CMD_SPEED_PID_PROPORTIONAL, 1, &data) != MSG_STATE_SUCCESS) {
         printf("fail 4");
     }
 
-    container.c_uint8 = 20;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_SPEED_PID_INTEGRAL,
-                          CMD_TYPE_WRITE, 1, &container) != MSG_STATE_SUCCESS) {
+    data.c_uint8 = 20;
+    if (ctrl_msgq(CMD_SPEED_PID_INTEGRAL, 1, &data) != MSG_STATE_SUCCESS) {
         printf("fail 5");
     }
 
-    container.c_uint8 = 20;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_SPEED_PID_DIFFERENTAL,
-                          CMD_TYPE_WRITE, 1, &container) != MSG_STATE_SUCCESS) {
+    data.c_uint8 = 20;
+    if (ctrl_msgq(CMD_SPEED_PID_DIFFERENTAL, 1, &data) != MSG_STATE_SUCCESS) {
         printf("fail 6");
     }
 
-    container.c_int16 = 150;
-    if (message_ctrlboard(msgq_id, MSGQ_ID, CMD_DESIRE_SPEED, CMD_TYPE_WRITE, 2,
-                          &container) != MSG_STATE_SUCCESS) {
+    data.c_int16 = 0;
+    if (ctrl_msgq(CMD_DESIRE_SPEED, 2, &data) != MSG_STATE_SUCCESS) {
         printf("fail 7");
     }
+
+    printf("Initialize finished.\n");
 }
