7098014fe1e81c463fa0bbea8ca0913da31a8250
unknownpgr <unknownpgr@gmail.com>
2020-09-08 21:29:58 +0900

    Fix run.sh

--- run.sh
+++ run.sh
@@ -11,10 +11,14 @@ echo "Copy the objects file to the remote device"
 scp "build/ctrlboard" root@10.10.70.4:"bin/"
 scp "build/recognize" root@10.10.70.4:"bin/"
 scp "build/clean-car" root@10.10.70.4:"bin/"
-scp "build/control"   root@10.10.70.4:"bin/"
+scp "build/process"   root@10.10.70.4:"bin/"
+
+# Because -e flag was set, initialization before start is required.
+echo "Initialize ipcs on the remote device."
+ssh -t root@10.10.70.4 "bin/clean-car"
 
 echo "Execute the programs on the remote device"
-ssh -t root@10.10.70.4 "bin/ctrlboard & bin/recognize & bin/control"
+ssh -t root@10.10.70.4 "bin/ctrlboard & bin/recognize & bin/process"
 
 echo "Clean the programs on the remote device."
 ssh -t root@10.10.70.4 "bin/clean-car"
