2aeac42ac9a444c3a02ae6f4f601a51332077dec
shythm <tjdgh2626@gmail.com>
2020-10-11 01:01:54 +0900

    Fix mission-obstacle

--- src/process/mission-obstacle.c
+++ src/process/mission-obstacle.c
@@ -17,20 +17,27 @@ void check_obstacle(State *state) {
             state->missions.obstacle.priority = 10;
             is_checked                        = true;
         }
-    } else {
-        state->input->stop_obstacle.enabled = false;
     }
 }
 
-#define ONE_SHOT_COUNT 10
+#define ONE_SHOT_COUNT 1000
 void do_obstacle(State *state) {
     ctrlboard_byte_container container;
 
     container.c_int16 = 0;
     send_ctrlboard(state->ctrl, CMD_DESIRE_SPEED, 2, &container);
 
+    container.c_uint8 = 50;
+    if (send_ctrlboard(state->ctrl, CMD_SOUND, 1, &container) !=
+        MSG_STATE_SUCCESS)
+        printf("fail to sound beep: mission-parking\n");
+
     // block until the obstacle is disappear.
     for (int i = 0; i < ONE_SHOT_COUNT; i++) {
         if (state->input->stop_obstacle.value.pos_x != -1) i = 0;
+        usleep(1000);
     }
+
+    state->input->stop_obstacle.enabled = false;
+    sleep(1);
 }
