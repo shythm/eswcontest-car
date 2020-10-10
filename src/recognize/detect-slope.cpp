6ab2254df14002b6d7c6352f1be69bbfefa0e16f
shythm <tjdgh2626@gmail.com>
2020-10-10 15:53:54 +0900

    Implement detect-slope

--- src/recognize/detect-slope.cpp
+++ src/recognize/detect-slope.cpp
@@ -3,7 +3,9 @@
 #include <opencv2/opencv.hpp>
 
 #define IMG_W    VPE_OUTPUT_W
+#define IMG_WC   IMG_W / 2
 #define IMG_H    VPE_OUTPUT_H
+#define IMG_HC   IMG_H / 2
 #define IMG_SIZE VPE_OUTPUT_IMG_SIZE
 
 using namespace cv;
@@ -26,12 +28,27 @@ void makellMask(unsigned char *mask, int w, int h, int offset, int size) {
     }
 }
 
+bool intersection(Point2f o1, Point2f p1, Point2f o2, Point2f p2, Point2f &r) {
+    Point2f x  = o2 - o1;
+    Point2f d1 = p1 - o1;
+    Point2f d2 = p2 - o2;
+
+    float cross = d1.x * d2.y - d1.y * d2.x;
+    if (abs(cross) < 1e-8) return false;
+
+    double t1 = (x.x * d2.y - x.y * d2.x) / cross;
+    r         = o1 + d1 * t1;
+    return true;
+}
+
+#define INTER_Y_THRES 60
+
 bool detectSlope(recog_arg *arg) {
     unsigned char *cam_out = new unsigned char[IMG_SIZE];
     copy(arg->camera_output, arg->camera_output + IMG_SIZE, cam_out);
-    Mat frame(IMG_H, IMG_W, CV_8UC3, cam_out);
-    Mat disp(IMG_H, IMG_W, CV_8UC3, arg->display_input);
-    int ret = 0;
+    Mat  frame(IMG_H, IMG_W, CV_8UC3, cam_out);
+    Mat  disp(IMG_H, IMG_W, CV_8UC3, arg->display_input);
+    bool ret = false;
 
     // HSV로 변환
     cvtColor(frame, frame, cv::COLOR_BGR2HSV);
@@ -46,7 +63,7 @@ bool detectSlope(recog_arg *arg) {
     static bool          init_mask = true;
     static Mat           mmask(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC1, mask);
     if (init_mask) {
-        makellMask(mask, IMG_W, IMG_H, 10, 40);
+        makellMask(mask, IMG_W, IMG_H, 0, 40);
         init_mask = false;
     }
 
@@ -60,44 +77,53 @@ bool detectSlope(recog_arg *arg) {
     vector<Vec4i> lines;
     HoughLinesP(frame, lines, 1, CV_PI / 180, 20, 30, 3);
 
-    // 검출 정보 화면 출력
-    cvtColor(frame, frame, COLOR_GRAY2BGR);
-    if (lines.size()) {
-        float slope_l_sum = 0, slope_r_sum = 0;
-        int   slope_l_cnt = 0, slope_r_cnt = 0;
-
-        for (Vec4i l : lines) {
-            line(frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255),
-                 2);
-
-            float w = l[0] - l[2];
-            float h = l[3] - l[1];
-            if (w) {
-                float s = (double)h / w;
-                if ((l[0] < IMG_W / 2) && (l[2] < IMG_W / 2)) { // 왼쪽 차선
-                    slope_l_sum += s;
-                    slope_l_cnt++;
-                } else if ((l[0] >= IMG_W / 2) &&
-                           ((l[0] >= IMG_W / 2))) { // 오른쪽 차선
-                    slope_r_sum += s;
-                    slope_r_cnt++;
-                }
-            }
-        }
-
-        if (slope_l_cnt) {
-            putText(frame, "L: " + std::to_string(slope_l_sum / slope_l_cnt),
-                    Point(25, 25), FONT_HERSHEY_SIMPLEX, 1,
-                    Scalar(255, 255, 255), 2);
-        }
-        if (slope_r_cnt) {
-            putText(frame, "R: " + std::to_string(slope_r_sum / slope_r_cnt),
-                    Point(25, 60), FONT_HERSHEY_SIMPLEX, 1,
-                    Scalar(255, 255, 255), 2);
+    // 가장 아래에 있는 두 직선 검출
+    Vec4i line_l(0, 0, 0, 0);
+    Vec4i line_r(0, 0, 0, 0);
+    for (Vec4i l : lines) {
+        if (l[0] < IMG_WC && l[2] < IMG_WC) { // 왼쪽 차선
+            if (min(line_l[1], line_l[3]) < min(l[1], l[3])) line_l = l;
+        } else if (l[0] >= IMG_WC && l[2] >= IMG_WC) { // 오른쪽 차선
+            if (min(line_r[1], line_r[3]) < min(l[1], l[3])) line_r = l;
         }
     }
 
-    // cvtColor(frame, frame, COLOR_GRAY2BGR);
+    // 두 직선의 교점 구하기
+    Vec4i   vi(1, 1, 1, 1);
+    Point2f r(0, 0);
+    if (line_l.dot(vi) && line_r.dot(vi)) { // 두 직선이 유효하면
+        Point2f o1(line_l[0], line_l[1]);
+        Point2f p1(line_l[2], line_l[3]);
+        Point2f o2(line_r[0], line_r[1]);
+        Point2f p2(line_r[2], line_r[3]);
+        intersection(o1, p1, o2, p2, r);
+
+        // y 좌표의 절댓값이 일정 수준 이상이면 경사로로 판단
+        cout << r.y << endl;
+        if (r.y <= -INTER_Y_THRES) ret = true;
+    }
+
+    // 검출 정보 화면 출력
+    cvtColor(frame, frame, COLOR_GRAY2BGR);
+#if 0
+    if (r.dot(Point2f(1, 1))) {
+        putText(frame, to_string(r.x), Point(25, 25), FONT_HERSHEY_SIMPLEX, 1,
+                Scalar(255, 255, 255), 2);
+        putText(frame, to_string(r.y), Point(25, 60), FONT_HERSHEY_SIMPLEX, 1,
+                Scalar(255, 255, 255), 2);
+        line(frame, r, r, Scalar(0, 0, 255), 10);
+    }
+
+    if (line_l.dot(Vec4i(1, 1, 1, 1))) {
+        line(frame, Point(line_l[0], line_l[1]), Point(line_l[2], line_l[3]),
+             Scalar(0, 0, 255), 2);
+    }
+
+    if (line_r.dot(Vec4i(1, 1, 1, 1))) {
+        line(frame, Point(line_r[0], line_r[1]), Point(line_r[2], line_r[3]),
+             Scalar(0, 0, 255), 2);
+    }
+#endif
     frame.copyTo(disp);
 
     delete[] cam_out;
