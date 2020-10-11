c6dc82ac2e81346be3f97b6a6779c304fcdbee61
unknownpgr <unknownpgr@gmail.com>
2020-10-12 00:11:24 +0900

    Add white detection algorithm

--- src/recognize/detect-is-there-car.cpp
+++ src/recognize/detect-is-there-car.cpp
@@ -1,5 +1,6 @@
 #include "detect-is-there-car.h"
 #include "recognize-lib.h"
+#include <math.h>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -13,11 +14,34 @@ vector_car_pos detectIsThereCar(recog_arg *arg) {
 
     Mat temp;
     cvtColor(src, temp, COLOR_BGR2GRAY);
+
+    int    y, x;
+    float  cur, max;
+    uchar *yPtr;
+    max = 0;
+#define DIVIDER 32.f
+    // Find max
+    for (y = 0; y < VPE_OUTPUT_H; y++) {
+        yPtr = temp.ptr<uchar>(y);
+        for (x = 0; x < VPE_OUTPUT_W / 2; x++) {
+            cur = exp((float)(yPtr[x]) / DIVIDER);
+            if (cur > max) max = cur;
+        }
+    }
+
+    for (y = 0; y < VPE_OUTPUT_H; y++) {
+        yPtr = temp.ptr<uchar>(y);
+        for (x = 0; x < VPE_OUTPUT_W / 2; x++) {
+            cur     = exp((float)(yPtr[x]) / DIVIDER) * 255 / max;
+            yPtr[x] = (char)(cur);
+        }
+    }
+
     cvtColor(temp, temp, COLOR_GRAY2BGR);
 
     // show image on display
     Mat car_disp_img(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC3, arg->display_input);
-    temp.copyTo(car_disp_img);
+    // temp.copyTo(car_disp_img);
 
     // Car position
     vector_car_pos ret;
