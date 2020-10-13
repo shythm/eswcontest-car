deb939daab23ff9ae9629b32edeeecbd393f7593
shythm <tjdgh2626@gmail.com>
2020-10-14 00:27:48 +0900

    Enhance mission-obstacle behavior

--- src/process/mission-obstacle.c
+++ src/process/mission-obstacle.c
@@ -1,36 +1,53 @@
+#include "car-header.h"
 #include "ctrlboard-lib.h"
 #include "process.h"
-
 void do_obstacle(State *state);
 
+#define THRESHOLD_SO_AREA 1000.0f
+#define ONE_SHOT_COUNT    250
+#define ONE_SHOT_DELAY_US 1000
+#define WAIT_DELAY        2
+
+static recog_stop_obstacle_data *so_data;
+
 void init_obstacle(State *state) {
-    state->input->stop_obstacle.enabled     = true;
-    state->input->stop_obstacle.value.pos_x = -1;
-    state->missions.obstacle.function       = do_obstacle;
+    so_data             = &(state->input->stop_obstacle);
+    so_data->value.area = 0.0f;
+    so_data->enabled    = true;
+
+    state->missions.obstacle.function = do_obstacle;
 }
 
 void check_obstacle(State *state) {
     static int is_checked = false;
 
     if (!is_checked) {
-        if (state->input->stop_obstacle.value.pos_x != -1) {
-            state->missions.obstacle.priority = 10;
-            is_checked                        = true;
+        if (so_data->value.area > THRESHOLD_SO_AREA) {
+            state->missions.obstacle.priority = 3;
+
+            is_checked = true;
         }
-    } else {
-        state->input->stop_obstacle.enabled = false;
     }
 }
 
-#define ONE_SHOT_COUNT 10
 void do_obstacle(State *state) {
     ctrlboard_byte_container container;
 
     container.c_int16 = 0;
     send_ctrlboard(state->ctrl, CMD_DESIRE_SPEED, 2, &container);
 
+    beep(state->ctrl, 50);
+
     // block until the obstacle is disappear.
     for (int i = 0; i < ONE_SHOT_COUNT; i++) {
-        if (state->input->stop_obstacle.value.pos_x != -1) i = 0;
+        if (so_data->value.area > THRESHOLD_SO_AREA) i = 0;
+        usleep(ONE_SHOT_DELAY_US);
     }
+
+    // wait while the priority stop obstacle is dissapeared.
+    sleep(WAIT_DELAY);
+
+    // restore and clear
+    init_drive(state);
+    so_data->enabled = false;
 }
