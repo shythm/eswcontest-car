e8e10c14ea430602d5ba99b0687a6041a10be102
moran991231 <moran991231@naver.com>
2020-11-08 14:03:28 +0900

    preparing_update

--- src/process/mission-overtaking.c
+++ src/process/mission-overtaking.c
@@ -1,13 +1,30 @@
+#include "ctrlboard-lib.h"
 #include "process.h"
 
-void do_overtaking(State *state) {}
+typedef ctrlboard_byte_container container;
+
+void do_overtaking(State *state);
 
 void init_overtaking(State *state) {
-    state->input->is_on_stop_line.enabled = true;
-    state->missions.overtaking.function   = do_overtaking;
+    state->input->is_there_car.enabled  = true;
+    state->missions.overtaking.function = do_overtaking;
 }
 
 void check_overtaking(State *state) {
-    if (state->input->is_on_stop_line.value == true)
-        state->missions.overtaking.priority = 2;
+    // if (state->input->is_on_stop_line.value == true)
+    state->missions.overtaking.priority = 10;
+    printf("check-ov\n");
+    container data;
+    data.c_int16 = 1700;
+    if (send_ctrlboard(state->ctrl, CMD_CAMERA_Y_SERVO_CONTROL, 2, &data) !=
+        MSG_STATE_SUCCESS)
+        printf("fail to set steering : mission-parking ");
+}
+
+void do_overtaking(State *state) {
+    sleep(1);
+    // container data = {50};
+    // if (send_ctrlboard(state->ctrl, CMD_SOUND, 1, &data) !=
+    // MSG_STATE_SUCCESS)
+    //     printf("fail to sound beep: mission-parking\n");
 }
--- src/recognize/detect-is-there-car.cpp
+++ src/recognize/detect-is-there-car.cpp
@@ -2,6 +2,8 @@
 #include "recognize-lib.h"
 #include <opencv2/opencv.hpp>
 
+#define ABS(x) (((x) > 0) ? (x) : -(x))
+
 using namespace cv;
 using namespace std;
 
@@ -11,12 +13,52 @@ vector_car_pos detectIsThereCar(recog_arg *arg) {
     // Mat            output_img();
     //이미지 받아와서 mat 클래스로 감싼다
     static unsigned char cam_copied[VPE_OUTPUT_W * VPE_OUTPUT_H * 3];
+
     copy(cam_data, cam_data + sizeof(cam_copied), cam_copied);
     Mat src_img(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC3, cam_copied);
+    Mat gray_img = Mat::zeros(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC1);
+    cvtColor(src_img, gray_img, COLOR_BGR2GRAY);
+    Mat gray_roi1 = gray_img.clone();
+
+    // mask
+    for (int y = gray_img.rows / 5; y < (int)(gray_img.rows * 3.f / 5.f); y++) {
+        for (int x = 0; x < gray_img.cols; x++) {
+            gray_roi1.at<uchar>(y, x) = 0;
+        }
+    }
+
+    // edge
+    Mat           edge_img;
+    vector<Vec2f> lines;
+    Canny(gray_roi1, edge_img, 50, 100);
+    HoughLines(edge_img, lines, 1, CV_PI / 180, 250);
 
     // show image on display
     Mat car_disp_img(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC3, disp_data);
     src_img.copyTo(car_disp_img);
+
+    // make boa
+    unsigned char boa[100];
+    for (int i = 0; i < 100; i++) boa[i] = -ABS((255.f / 50) * (i - 50)) + 255;
+
+    Mat  boa_mat(1, 100, CV_8UC1, boa);
+    long boa_l = 0, boa_c = 0, boa_r = 0;
+    Mat  gray_l =
+        gray_img(Range(gray_img.rows / 5, (int)(gray_img.rows * 3.f / 5.f)),
+                 Range(0, 99));
+    Mat gray_c =
+        gray_img(Range(gray_img.rows / 5, (int)(gray_img.rows * 3.f / 5.f)),
+                 Range(110, 209));
+    Mat gray_r =
+        gray_img(Range(gray_img.rows / 5, (int)(gray_img.rows * 3.f / 5.f)),
+                 Range(220, 319));
+
+    for (int i = 0; i < gray_l.rows; i++) {
+        boa_l += boa_mat.dot(gray_l.row(i));
+        boa_c += boa_mat.dot(gray_c.row(i));
+        boa_r += boa_mat.dot(gray_r.row(i));
+    }
+    printf("L: %ld, C: %ld, R: %ld\n", boa_l, boa_c, boa_r);
 
     vector_car_pos ret;
 
