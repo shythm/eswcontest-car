b37a4a93ee29dc6d525d9986a0f8cdcf308a18e0
moran991231 <moran991231@naver.com>
2020-12-05 20:33:46 +0900

    add endfif

--- src/recognize/detect-lane.cpp
+++ src/recognize/detect-lane.cpp
@@ -226,7 +226,7 @@ void detectLane(recog_arg *arg, vector_lane *result) {
 
     // Restore size
     resize(img, img, sizeOrigin, INTER_NEAREST);
-
+#endif
     // // Copy processed image to display
     // copy(img.data, img.data + W * H * 3, arg->display_input);
 
