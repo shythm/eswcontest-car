741eaab65299e37f00970d18d54d70bbeff1ccd5
shythm <tjdgh2626@gmail.com>
2020-12-29 17:38:03 +0900

    Set to default code

--- src/process/mission-test.c
+++ src/process/mission-test.c
@@ -9,14 +9,4 @@ bool check_test(fnRun_t *fnRun) {
     return true;
 }
 
-void init_test(fnCheck_t *fnCheck) {
-    while (!recog->psd.valid) {};
-    while (1) {
-        printf("left1 %3.1f [cm] right2 %3.1f[cm] \n",
-               recog->psd.value[PSD_LEFT_1], recog->psd.value[PSD_RIGHT_1]);
-        fflush(stdout);
-        usleep(50000);
-    }
-
-    *fnCheck = check_test;
-}
+void init_test(fnCheck_t *fnCheck) { *fnCheck = check_test; }
